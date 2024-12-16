f3e1bcbbace5141ec99701f33fd34b93531ccaf0
Andre Kramer <andre.equus@gmail.com>
2024-12-16 10:55:57 +0000

    split into procs

--- multi-llm.py
+++ multi-llm.py
@@ -12,16 +12,16 @@ import openai
 import grok
 import llama
 
-# Which models to query:
+# Which models to query
 schedule = {
   "gemini": True,
   "claud": False,
-  "openai": False,
+  "openai": True,
   "grok": False,
-  "llama": False
+  "llama": True
 }
 
-# Where in the json to find the text reply:
+# Where in the json to find the text reply
 text_field = {
   "gemini": "text",
   "claud": "text",
@@ -30,7 +30,7 @@ text_field = {
   "llama": "content"
 }
 
-# model versions to use for queries:
+# model versions to use for queries
 models = {
   "gemini": "gemini-1.5-flash-latest",
   "claud": "claude-3-5-sonnet-20241022",
@@ -39,25 +39,18 @@ models = {
   "llama":  "llama3.3-70b"
 }
 
-# The order of results (skiping any not in schedule)
+# The order of responses (skiping any not in schedule)
 order = ["gemini", "claud", "openai", "grok", "llama"]
 
+# Push down the model configuration to imported models
 gemini.model = models["gemini"]
 claud.model = models["claud"]
 openai.model = models["openai"]
 grok.model = models["grok"]
 llama.model = models["llama"]
 
-async def main():
-  
-  if len(sys.argv) > 1:
-    prompt = sys.argv[1].replace("\n", "\\n");
-  else:
-    print("No command-line arguments provided.")
-    exit()
-
-  start_time = time.time()
-
+async def multi_way_query(prompt):
+  """Query the configured models in parallel and gather the responses. """
   promises = []
   async with aiohttp.ClientSession() as session:
     if schedule["gemini"]:
@@ -80,23 +73,70 @@ async def main():
       llama_promise = llama.ask(session, llama.make_query(prompt))
       promises.append(llama_promise)
 
-    results = await asyncio.gather(*promises)
+    responses = await asyncio.gather(*promises)
+  
+  return responses
 
-    i = 0
-    for o in order:
-      if not schedule[o]:
-        print("skiped " + o)
-        continue
-      print("model " + o)
-      json_data = json.loads(results[i])
-      i += 1
-      json_formatted_str = json.dumps(json_data, indent=2)
-      print(json_formatted_str)
-      text = support.search_json(json_data, text_field[o])
-      if text != None:
-        print(text)
-      else: 
-        print("No text found")
+def clean(str):
+  return str.replace("\n", "\\n")
+
+def parse_responses(responses, display=False):
+  """Parsing out the model specific text field. Display responses if display flag is True"""
+  response_texts = []
+  i = 0
+  for o in order:
+    if not schedule[o]:
+      if display: print("skiped " + o)
+      continue
+    print("model " + o)
+    json_data = json.loads(responses[i])
+    i += 1
+    json_formatted_str = json.dumps(json_data, indent=2)
+    print(json_formatted_str)
+    text = support.search_json(json_data, text_field[o])
+    if text != None:
+      if display: print(text)
+      response_texts.append(text)
+    else:
+      if display: print("No text found")
+
+  return response_texts
+
+async def compare_three_way(response_texts):
+  """Compare the first 3 reult texts. Return None if no matches"""
+  if len(response_texts) < 3:
+    print("Not enough responses to compare")
+    return None
+  
+  # 3 way comparison is possible
+  alice = clean(response_texts[0])
+  bob = clean(response_texts[1])
+  eve = clean(response_texts[0])
+
+  async with aiohttp.ClientSession() as session:
+    pass
+
+  return None
+
+async def main():
+  
+  if len(sys.argv) > 1:
+    prompt = clean(sys.argv[1]);
+  else:
+    print("Usage: python3 multi-llm.py query")
+    exit()
+
+  start_time = time.time()
+
+  responses = await multi_way_query(prompt)
+
+  texts = parse_responses(responses, True)
+
+  compared_text = compare_three_way(texts)
+
+  if compared_text is not None:
+    print("compared response")
+    print(compared_text)
 
   end_time = time.time()
   print(f"Time taken: {end_time - start_time:.2f} seconds")
