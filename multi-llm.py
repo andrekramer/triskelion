7928629b178363f8831af0b6e12deebad3efe2d0
Andre Kramer <andre.equus@gmail.com>
2024-12-17 18:35:16 +0000

    parallel comparisons

--- multi-llm.py
+++ multi-llm.py
@@ -40,7 +40,7 @@ def parse_responses(responses, display=False):
     json_data = json.loads(responses[i])
     i += 1
     json_formatted_str = json.dumps(json_data, indent=2)
-    print(json_formatted_str)
+    # print(json_formatted_str)
     text = support.search_json(json_data, model.text_field)
     if text != None and text.strip() != "":
       if display: print(text)
@@ -53,23 +53,23 @@ def parse_responses(responses, display=False):
 
 
 async def compare(session, model, comparison, verbose):
-    query = model.make_query(clean(comparison))
-    # print(query)
-    response = await model.ask(session, query)
-    json_data = json.loads(response)
-    if verbose:
-      json_formatted_str = json.dumps(json_data, indent=2)
-      print(json_formatted_str)
-    text = support.search_json(json_data, model.text_field)
-    if text is None:
-      print("comparison failed")
-      return False
-    print("Comparison result:\n" + text)
+  query = model.make_query(clean(comparison))
+  # print(query)
+  response = await model.ask(session, query)
+  json_data = json.loads(response)
+  if verbose:
+    json_formatted_str = json.dumps(json_data, indent=2)
+    print(json_formatted_str)
+  text = support.search_json(json_data, model.text_field)
+  if text is None:
+    print("comparison failed")
+    return False
+  print("comparison result:\n" + text)
 
-    if text.find("YES") != -1 and not text.find("NO") != -1:
-      return True
-    else:
-      return False
+  if text.find("YES") != -1 and not text.find("NO") != -1:
+    return True
+  else:
+    return False
 
 async def compare_one_way(response_texts, verbose=False):
   """Compare the first two non blank result texts. Return None if no matches"""
@@ -144,6 +144,66 @@ async def compare_two_or_three_way(response_texts, two_way_only=False, verbose=F
           
     return None
 
+async def compare_all(response_texts, verbose=False):
+  """Compare the first 3 non blank result texts in parallel"""
+  texts = [item for item in response_texts if item.strip() != ""]
+  if len(texts) < 3:
+    print("Not enough responses to compare")
+    return None
+ 
+  alice = texts[0]
+  bob = texts[1]
+  eve = texts[2]
+
+  comparison1 = "Alice says:\n" + alice + "\n" + \
+                "\nBob says:\n" + bob + "\n" + \
+                compare_instructions
+  if verbose: print(clean(comparison1))
+
+  comparison2 = "Alice says:\n" + alice + "\n" + \
+                "\nEve says:\n" + eve + "\n" + \
+                compare_instructions
+  if verbose: print(clean(comparison2))
+  
+  comparison3 = "Bob says:\n" + bob + "\n" + \
+                "\nEve says:\n" + eve + "\n" + \
+                compare_instructions
+  if verbose: print(clean(comparison3))
+ 
+  async with aiohttp.ClientSession() as session:
+    promises = []
+    model = comparison_models[0]
+    promise = compare(session, model, comparison1, verbose)
+    promises.append(promise)
+
+    model = comparison_models[1]
+    promise = compare(session, model, comparison2, verbose)
+    promises.append(promise)
+
+    model = comparison_models[2]
+    promise = compare(session, model, comparison3, verbose)
+    promises.append(promise)
+
+    responses = await asyncio.gather(*promises)
+
+  print("Alice and Bob " +  ("agree" if responses[0] else "disagree"))
+  print("Alice and Eve " +  ("agree" if responses[1] else "disagree"))
+  print("Bob and Eve " +  ("agree" if responses[2] else "disagree"))
+
+  if all(responses):
+    print("concensus")
+
+  if responses[0]:
+    return alice
+  
+  if responses[1]:
+    return alice
+  
+  if responses[2]:
+    return bob
+  
+  return None
+
 
 async def main():
 
@@ -151,7 +211,7 @@ async def main():
     action = sys.argv[1]
     prompt = clean(sys.argv[2])
   else:
-    print("Usage: python3 multi-llm.py 3-way|2-way|1-way query")
+    print("Usage: python3 multi-llm.py 3-way|2-way|1-way|all query")
     exit()
 
   configure()
@@ -170,6 +230,8 @@ async def main():
     compared_text = await compare_two_or_three_way(texts, True)
   elif action == "3-way":
     compared_text = await compare_two_or_three_way(texts, False)
+  elif action == "all":
+    compared_text = await compare_all(texts)
   else:
     print("unknown action " + action)
 
